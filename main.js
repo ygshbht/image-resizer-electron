8e3d330b611a24be901319dc4fe42d2ebeac2a69
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-04-30 16:13:12 +0530

    Related to publshing

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const resizeImg = require('resize-img');
-const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
-const isDev = process.env.NODE_ENV !== 'production';
-const isMac = process.platform === 'darwin';
+// Determine which .env file to use based on NODE_ENV
+const envFile =
+  process.env.NODE_ENV === "production" ? ".env.prod" : ".env.dev";
+
+require("dotenv").config({ path: envFile });
+
+const resizeImg = require("resize-img");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+
+const isDev = process.env.NODE_ENV === "development";
+const isMac = process.platform === "darwin";
 
 let mainWindow;
 let aboutWindow;
@@ -20,7 +27,7 @@ function createMainWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
@@ -29,8 +36,8 @@ function createMainWindow() {
     mainWindow.webContents.openDevTools();
   }
 
-    // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+  // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 // About Window
@@ -38,22 +45,22 @@ function createAboutWindow() {
   aboutWindow = new BrowserWindow({
     width: 300,
     height: 300,
-    title: 'About Electron',
+    title: "About Electron",
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
   });
 
-   aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
 }
 
 // When the app is ready, create the window
-app.on('ready', () => {
+app.on("ready", () => {
   createMainWindow();
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
   // Remove variable from memory
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on("closed", () => (mainWindow = null));
 });
 
 // Menu template
@@ -64,7 +71,7 @@ const menu = [
           label: app.name,
           submenu: [
             {
-              label: 'About',
+              label: "About",
               click: createAboutWindow,
             },
           ],
@@ -72,15 +79,15 @@ const menu = [
       ]
     : []),
   {
-    role: 'fileMenu',
+    role: "fileMenu",
   },
   ...(!isMac
     ? [
         {
-          label: 'Help',
+          label: "Help",
           submenu: [
             {
-              label: 'About',
+              label: "About",
               click: createAboutWindow,
             },
           ],
@@ -100,12 +107,12 @@ const menu = [
   ...(isDev
     ? [
         {
-          label: 'Developer',
+          label: "Developer",
           submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
-            { type: 'separator' },
-            { role: 'toggledevtools' },
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
           ],
         },
       ]
@@ -113,9 +120,9 @@ const menu = [
 ];
 
 // Respond to the resize image event
-ipcMain.on('image:resize', (e, options) => {
+ipcMain.on("image:resize", (e, options) => {
   // console.log(options);
-  options.dest = path.join(os.homedir(), 'imageresizer');
+  options.dest = path.join(os.homedir(), "imageresizer");
   resizeImage(options);
 });
 
@@ -142,7 +149,7 @@ async function resizeImage({ imgPath, height, width, dest }) {
     fs.writeFileSync(path.join(dest, filename), newPath);
 
     // Send success to renderer
-    mainWindow.webContents.send('image:done');
+    mainWindow.webContents.send("image:done");
 
     // Open the folder in the file explorer
     shell.openPath(dest);
@@ -152,11 +159,11 @@ async function resizeImage({ imgPath, height, width, dest }) {
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) app.quit();
 });
 
 // Open a window if none are open (macOS)
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
 });
