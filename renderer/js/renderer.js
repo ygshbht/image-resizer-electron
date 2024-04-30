9b578ec59f3ec59e63d228f63e7e73462b47c291
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-04-30 10:47:56 +0530

    Proportional Resizing

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,9 +1,62 @@
-const form = document.querySelector('#img-form');
-const img = document.querySelector('#img');
-const outputPath = document.querySelector('#output-path');
-const filename = document.querySelector('#filename');
-const heightInput = document.querySelector('#height');
-const widthInput = document.querySelector('#width');
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const filename = document.querySelector("#filename");
+const heightInput = document.querySelector("#height");
+const widthInput = document.querySelector("#width");
+
+let isProportionalBtnActive = true;
+
+const proportionalBtn = document.querySelector("#proportional-button");
+let widthValue = 0;
+let heightValue = 0;
+
+let proportion = 1;
+
+proportionalBtn.addEventListener("click", toggleProportionalButton);
+
+widthInput.addEventListener("input", handleInputChange);
+heightInput.addEventListener("input", handleInputChange);
+
+function handleInputChange(e) {
+  const newValue = parseInt(e.target.value);
+
+  if (!newValue || isNaN(newValue)) {
+    widthValue = 0;
+    heightValue = 0;
+    widthInput.value = widthValue;
+    heightInput.value = heightValue;
+  }
+
+  if (e.target === heightInput) {
+    if (isProportionalBtnActive) {
+      const newWidth = parseInt(newValue * proportion);
+      widthInput.value = parseInt(newWidth);
+      widthValue = newWidth;
+    }
+    heightValue = newValue;
+  } else {
+    if (isProportionalBtnActive) {
+      const newHeight = parseInt(newValue / proportion);
+      heightInput.value = parseInt(newHeight);
+      heightValue = newHeight;
+    }
+    widthValue = newValue;
+  }
+}
+
+function toggleProportionalButton(e) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  const btn = proportionalBtn;
+  if (isProportionalBtnActive) {
+    btn.classList.remove("active");
+  } else {
+    btn.classList.add("active");
+    proportion = widthValue / heightValue;
+  }
+  isProportionalBtnActive = !isProportionalBtnActive;
+}
 
 // Load image and show form
 function loadImage(e) {
@@ -11,7 +64,7 @@ function loadImage(e) {
 
   // Check if file is an image
   if (!isFileImage(file)) {
-    alertError('Please select an image');
+    alertError("Please select an image");
     return;
   }
 
@@ -21,18 +74,22 @@ function loadImage(e) {
   image.onload = function () {
     widthInput.value = this.width;
     heightInput.value = this.height;
+
+    widthValue = parseInt(this.width);
+    heightValue = parseInt(this.height);
+    proportion = widthValue / heightValue;
   };
 
   // Show form, image name and output path
-  form.style.display = 'block';
+  form.style.display = "block";
   filename.innerHTML = img.files[0].name;
-  outputPath.innerText = path.join(os.homedir(), 'imageresizer');
+  outputPath.innerText = path.join(os.homedir(), "imageresizer");
 }
 
 // Make sure file is an image
 function isFileImage(file) {
-  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-  return file && acceptedImageTypes.includes(file['type']);
+  const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
+  return file && acceptedImageTypes.includes(file["type"]);
 }
 
 // Resize image
@@ -40,12 +97,12 @@ function resizeImage(e) {
   e.preventDefault();
 
   if (!img.files[0]) {
-    alertError('Please upload an image');
+    alertError("Please upload an image");
     return;
   }
 
-  if (widthInput.value === '' || heightInput.value === '') {
-    alertError('Please enter a width and height');
+  if (widthInput.value === "" || heightInput.value === "") {
+    alertError("Please enter a width and height");
     return;
   }
 
@@ -54,7 +111,7 @@ function resizeImage(e) {
   const width = widthInput.value;
   const height = heightInput.value;
 
-  ipcRenderer.send('image:resize', {
+  ipcRenderer.send("image:resize", {
     imgPath,
     height,
     width,
@@ -62,7 +119,7 @@ function resizeImage(e) {
 }
 
 // When done, show message
-ipcRenderer.on('image:done', () =>
+ipcRenderer.on("image:done", () =>
   alertSuccess(`Image resized to ${heightInput.value} x ${widthInput.value}`)
 );
 
@@ -72,9 +129,9 @@ function alertSuccess(message) {
     duration: 5000,
     close: false,
     style: {
-      background: 'green',
-      color: 'white',
-      textAlign: 'center',
+      background: "green",
+      color: "white",
+      textAlign: "center",
     },
   });
 }
@@ -85,14 +142,14 @@ function alertError(message) {
     duration: 5000,
     close: false,
     style: {
-      background: 'red',
-      color: 'white',
-      textAlign: 'center',
+      background: "red",
+      color: "white",
+      textAlign: "center",
     },
   });
 }
 
 // File select listener
-img.addEventListener('change', loadImage);
+img.addEventListener("change", loadImage);
 // Form submit listener
-form.addEventListener('submit', resizeImage);
+form.addEventListener("submit", resizeImage);
